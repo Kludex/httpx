f71dc19f972a446cb58e4e0e8995397f98876137
florimondmanca <florimond.manca@protonmail.com>
2023-02-13 00:57:24 +0100

    Add support for streaming in ASGIDispatch

--- tests/test_asgi.py
+++ tests/test_asgi.py
@@ -1,4 +1,5 @@
 import json
+from contextlib import aclosing
 
 import pytest
 
@@ -55,6 +56,20 @@ async def echo_headers(scope, receive, send):
 
     await send({"type": "http.response.start", "status": status, "headers": headers})
     await send({"type": "http.response.body", "body": output})
+
+
+async def hello_world_endlessly(scope, receive, send):
+    status = 200
+    output = b"Hello, World!"
+    headers = [(b"content-type", "text/plain"), (b"content-length", str(len(output)))]
+
+    await send({"type": "http.response.start", "status": status, "headers": headers})
+
+    k = 0
+    while True:
+        body = b"%d: %s\n" % (k, output)
+        await send({"type": "http.response.body", "body": body, "more_body": True})
+        k += 1
 
 
 async def raise_exc(scope, receive, send):
@@ -201,3 +216,38 @@ async def test_asgi_exc_no_raise():
         response = await client.get("http://www.example.org/")
 
         assert response.status_code == 500
+
+async def test_asgi_streaming():
+    client = httpx.AsyncClient(app=hello_world_endlessly)
+    async with client.stream("GET", "http://www.example.org/") as response:
+        assert response.status_code == 200
+        lines = []
+
+        async with aclosing(response.aiter_lines()) as stream:
+            async for line in stream:
+                if line.startswith("3: "):
+                    break
+                lines.append(line)
+
+        assert lines == [
+            "0: Hello, World!\n",
+            "1: Hello, World!\n",
+            "2: Hello, World!\n",
+        ]
+
+
+@pytest.mark.anyio
+async def test_asgi_streaming_exc():
+    client = httpx.AsyncClient(app=raise_exc)
+    with pytest.raises(RuntimeError):
+        async with client.stream("GET", "http://www.example.org/"):
+            pass  # pragma: no cover
+
+
+@pytest.mark.anyio
+async def test_asgi_streaming_exc_after_response():
+    client = httpx.AsyncClient(app=raise_exc_after_response)
+    with pytest.raises(RuntimeError):
+        async with client.stream("GET", "http://www.example.org/") as response:
+            async for _ in response.aiter_bytes():
+                pass  # pragma: no cover
